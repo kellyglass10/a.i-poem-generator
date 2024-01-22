d89a2cb8c4b4c4b346e19db5fb0e90e516bb3370
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-01-22 10:00:33 -0800

    added-basic-form-handling
    added typewriter js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "A romance so sweet, a tale of two hearts; A love that never fades or falls apart",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
